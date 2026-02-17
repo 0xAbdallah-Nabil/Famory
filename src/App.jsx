0a817f752383120cd3e84ed56560ae25863d4408
abdallah nabil <abdallahnabil2003@gmail.com>
2026-02-17 22:01:06 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/index.jsx";
 import Header from "./components/Header/index.jsx";
 import Footer from "./components/Footer/index.jsx";
@@ -11,7 +11,7 @@ import Checkout from "./Pages/Checkout/Checkout.jsx";
 import OurServices from "./Pages/OurServices/OurServices.jsx";
 function App() {
   return (
-    <Router >
+    <Router>
       <DiscountSection />
       <Header />
       <Routes>
